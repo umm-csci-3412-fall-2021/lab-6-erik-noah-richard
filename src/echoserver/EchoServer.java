393c53e939679c0a5fc334a112aabd6b0338919b
zeus-ctrl <noah.constable@gmail.com>
2021-10-28 10:56:09 -0500

    Implement thread pool using ExecutorService.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,11 +5,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class EchoServer {
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 6013; 
+	public static final int PORT_NUMBER = 6013;
+	public static final int NUM_THREADS = 4;
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
@@ -19,15 +23,18 @@ public class EchoServer {
 
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 
+		ExecutorService threadPool = Executors.newFixedThreadPool(NUM_THREADS);
+
 		while (true) {
 
 			Socket socket = serverSocket.accept();
 
 			EchoRunnable run = new EchoRunnable(socket);
 
-			Thread t = new Thread(run);
+			Thread newThread = new Thread(run);
 
-			t.start();
+			threadPool.submit(newThread);
+			// t.start();
 		}
 	}
 
