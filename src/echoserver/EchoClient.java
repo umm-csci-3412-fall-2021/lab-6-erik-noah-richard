4f23caf4bd9940e412bc8aa289841cc0fcca3afe
zeus-ctrl <noah.constable@gmail.com>
2021-10-28 10:28:01 -0500

    Implement EchoClient using mulithreading.
    Co-authored-by: RichardLussier <RichardLussier@users.noreply.github.com>
    Co-authored-by: ErikRauer <ErikRauer@users.noreply.github.com>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -18,6 +18,85 @@ public class EchoClient {
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+		UserInputProcessor userInput = new UserInputProcessor(socket, socketOutputStream);
+		ServerOutputProcessor serverOutput = new ServerOutputProcessor(socket, socketInputStream);
+
+		Thread userInputThread = new Thread(userInput);
+		Thread serverOutputThread = new Thread(serverOutput);
+		
+		userInputThread.start();
+		serverOutputThread.start();
+
+		try {
+			userInputThread.join();
+			serverOutputThread.join();
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException: " + e.getMessage());
+			System.exit(1);
+		}
+
+		socket.close();
+		
+	}
+
+	public class UserInputProcessor implements Runnable {
+
+		Socket socket;
+		OutputStream toServerStream;
+		
+		public UserInputProcessor(Socket socket, OutputStream socketOutputStream){
+			this.socket = socket;
+			this.toServerStream = socketOutputStream;
+		}
+
+		public void run	() {
+			try{
+				
+				int keyboardInput = System.in.read();
+				while (keyboardInput != -1){
+					toServerStream.write(keyboardInput);
+					keyboardInput = System.in.read();
+				}
+
+				toServerStream.flush();
+				socket.shutdownOutput();
+
+			} catch(IOException e) {
+				System.out.println("Error reading user input");
+				System.out.println(e);
+				System.exit(1);
+			}
+		}
+	}
+	
+	
+	public class ServerOutputProcessor implements Runnable {
+		
+		Socket socket;
+		InputStream fromServerStream;
+		
+		public ServerOutputProcessor(Socket socket, InputStream socketInputStream){
+			this.socket = socket;
+			this.fromServerStream = socketInputStream;
+		}
+
+		public void run	() {
+			try{
+				
+				int serverOutput = fromServerStream.read();
+				while (serverOutput != -1){
+					System.out.write(serverOutput);
+					serverOutput = fromServerStream.read();
+				}
+
+				System.out.flush();
+				socket.shutdownInput();
+
+			} catch(IOException e) {
+				System.out.println("Error reading user input");
+				System.out.println(e);
+				System.exit(1);
+			}
+		}
 	}
 }
